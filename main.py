e801e057cd0b2adbbee14da52f395d99019a6ed3
bobobo34 <81543088+bobobo34@users.noreply.github.com>
2022-10-19 22:47:26 -0400

    done

--- main.py
+++ main.py
@@ -3,6 +3,8 @@
 # Evin Lodder 10/19
 from turtle import *
 from math import sin, radians
+
+
 def draw_rec(x: float, y: float, width: float, height: float, fcol: str, bcol: str, centx: bool = True, centy: bool = False) -> None:
     penup()
     if centx:
@@ -20,8 +22,12 @@ def draw_rec(x: float, y: float, width: float, height: float, fcol: str, bcol: s
         fd(height)
         left(90)
     end_fill()
+
+
 def draw_iso_tri(x: float, y: float, length: float, fcol: str, bcol: str, centx: bool = True, centy: bool = False):
     draw_tri(x, y, length, [60, 60, 60], fcol, bcol, centx, centy)
+
+
 def draw_tri(x: float, y: float, length: float, angles: list[float], fcol: str, bcol: str, centx: bool = True, centy: bool = False):
     penup()
     if centx:
@@ -34,33 +40,138 @@ def draw_tri(x: float, y: float, length: float, angles: list[float], fcol: str, 
     begin_fill()
     pendown()
     ratio: float = length / (sin(radians(angles[1])))
-    print(f"{ratio=}")
     b: float = ratio * sin(radians(angles[0]))
-    print(f"{b=}")
     c: float = ratio * sin(radians(angles[2]))
-    print(f"{c=}")
     lengths: list[float] = [length, b, c]
     for i in range(3):
         fd(lengths[i])
         left(180 - angles[i])
-        print(f"going {lengths[i]} turning {180-angles[i]}")
     end_fill()
+
+
+def draw_rec_window(x: float, y: float, width: float, height: float):
+    draw_rec(x, y - height, width, height, "white", "black", False)
+    penup()
+    goto(x, y - height / 2)
+    pendown()
+    setheading(0)
+    fd(width)
+    penup()
+    goto(x + width / 2, y)
+    pendown()
+    setheading(270)
+    fd(height)
+    penup()
+    setheading(0)
+
+
 # Settings used for entire program
 tracer(0,0)
 hideturtle()
 width(2)
+
+# Background
+draw_rec(-1000, -125, 2000, 500, "light blue", "light blue", False)
+draw_rec(-1000, -625, 2000, 500, "green", "green", False)
+
+# Chimney
+draw_rec(-100, 100, 50, 200, "gray", "black")
+
+# Draw roof
+draw_tri(-25, 125, 550, [30, 120, 30], "maroon", "black")
+
 # Draw base rectangle segment
-draw_tri(0, 0, 100, [30, 120, 30], "maroon", "black", centy=True)
-#draw_rec(0, 0, 250, 200, "burlywood", "black", centy=True)
-#draw_rec(-225, -100, 200, 125, "sienna", "black")
-# Draw roof segment
-# Draw chimney
+draw_rec(-225, 0, 400, 250, "burlywood", "black", centx=False, centy=True)
+# Bricks
+width(1)
+penup()
+right(90)
+for j in range(10):
+    # First row of bricks
+    for i in range(-225, 185, 20):
+        goto(i, 125 - j * 25)
+        pendown()
+        color("black")
+        fd(12.5)
+        penup()
+    # Separating line between layers
+    goto(-225, (112.5 - j * 25))
+    pendown()
+    left(90)
+    fd(400)
+    right(90)
+    penup()
+    # Second row of bricks
+    for g in range(-215, 175, 20):
+        goto(g, 112.5 - j * 25)
+        pendown()
+        color("black")
+        fd(12.5)
+        penup()
+    # Second separating line to complete 2-brick-tall row
+    goto(-225, (100 - j * 25))
+    pendown()
+    left(90)
+    fd(400)
+    right(90)
+    penup()
+
+# Reset rotation and width
+left(90)
+width(2)
+
+# Garage
+draw_rec(-100, -125, 200, 125, "sienna", "black")
+draw_rec(-100, -110, 50, 2, "black", "black")
+
 # Draw door
+penup()
+draw_rec(65, -125, 50, 80, "brown", "black", centx=False)
+penup()
+goto(100, -90)
+pendown()
+dot(10, "gold")
+penup()
 # Draw circle window
+penup()
+goto(90, -20)
+pendown()
+
+# Fill window circle
+fillcolor("white")
+begin_fill()
+circle(25)
+end_fill()
+
+# Draw lines
+penup()
+goto(90, 30)
+pendown()
+setheading(270)
+fd(50)
+penup()
+
+goto(65, 5)
+pendown()
+setheading(0)
+fd(50)
+penup()
+
+
 # Draw top-center window
+draw_rec_window(-75, 100, 75, 50)
+
 # Draw top-left window
+draw_rec_window(-200, 100, 75, 50)
+
 # Draw top-right window
+draw_rec_window(50, 100, 75, 50)
+
 # Draw bottom-left window
+draw_rec_window(20, -50, 30, 30)
+
 # Draw bottom-right window
+draw_rec_window(130, -50, 30, 30)
+
 update()
 done()
